3bd0f2bfeae2a5b3d4e8b9cc116c56810f904599
Viktor Tullsten <viktor.tullsten@elev.ga.lbs.se>
2018-10-10 15:06:11 +0200

    Kommenterat på vad scripten gör

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -6,7 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     public Color color;
     public SpriteRenderer rend;
-    public float movespeed = 10f;
+    public float movespeed = 5f;
     public float time;
     // Use this for initialization
     void Start()
@@ -20,41 +20,39 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.R))
-        {
-            rend.color = new Color(1, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.G))
-        {
-            rend.color = new Color(0, 1, 0);
-        }
-
-        if (Input.GetKey(KeyCode.B))
-        {
-            rend.color = new Color(0, 0, 1f);
-        }
+    
+        
 
         
         {
             transform.Translate(0, movespeed * Time.deltaTime, 0, Space.Self);
+             
         }
+        // Planet åker frammåt konstant.
 
         if (Input.GetKey ( KeyCode.A))
         {
             transform.Rotate(0, 0, 100 * Time.deltaTime);
             rend.color = new Color(0, 1, 0f);
+            
         }
+        //Om knappen (A) trycks ned, svänger flygplanet till vänster och byter ferg till grön.
 
         if (Input.GetKey( KeyCode.D))
         {
             transform.Rotate(0, 0, -180 * Time.deltaTime);
             rend.color = new Color(0, 0, 1f);
-        }
+           
+        } 
+        //Om knappen (D) trycks ned, svänger flygplanet till höger och byter ferg till blå.
+
         if (Input.GetKey ( KeyCode.S))
         {
             transform.Translate(0, -movespeed/2  * Time.deltaTime, 0, Space.Self);
+             
         }
+        // Om knappen (S) trycks ned, Åker planet hälften så snabbt. 
+
         Timer();
 
     }
@@ -63,14 +61,10 @@ public class NewBehaviourScript : MonoBehaviour
         time += 1 * Time.deltaTime;
 
         Debug.Log(string.Format("timer: {0}", time));
-       
-    }
+      
+    } 
+    // Timer som räknar upp i secunder. 
     
-
-
-
-
-
 }
 
 
